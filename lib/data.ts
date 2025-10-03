fd2b8da9f575b64fb3c12e6c79b1be34910867eb
Nusaibul Hasan <125681843+Hasan8936@users.noreply.github.com>
2025-10-03 21:04:01 +0530

    fix(entities): unify imports to '@/entities' to resolve case-sensitive build errors

--- lib/data.ts
+++ lib/data.ts
@@ -1,7 +1,7 @@
-import ordersData from '../entities/Order.json';
-import partnersData from '../entities/DeliveryPartner.json';
-import locationsData from '../entities/Location.json';
-import surgePoliciesData from '../entities/SurgePolicy.json';
+import ordersData from '@/entities/Order.json';
+import partnersData from '@/entities/DeliveryPartner.json';
+import locationsData from '@/entities/Location.json';
+import surgePoliciesData from '@/entities/SurgePolicy.json';
 import { Order, DeliveryPartner, Location, SurgePolicy } from '@/types';
 
 // Type safe data exports
